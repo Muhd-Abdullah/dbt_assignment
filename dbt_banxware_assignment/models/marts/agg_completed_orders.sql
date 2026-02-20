f66cf5761ead7ec648684a37a64e7d6667533597
Muhd-Abdullah <muhammadabdullah.e1@gmail.com>
2026-02-20 16:54:02 +0100

    dbt assignment

--- dbt_banxware_assignment/models/marts/agg_completed_orders.sql
+++ dbt_banxware_assignment/models/marts/agg_completed_orders.sql
@@ -26,7 +26,7 @@
 -- -----------------------------------------------------------------------------
 {{ config(materialized='table') }}
 
-with orders as (
+with completed_orders as (
 
   select
     f.order_id,
@@ -34,6 +34,9 @@ with orders as (
     f.date_day,
     ROUND(SUM(f.total_sales_amount), 2) AS order_total_sales_amount
   from {{ ref('fct_transformed_sales_data') }} f
+  join {{ ref('dim_orders') }} ord
+    on f.order_id = ord.order_id
+  where ord.order_status = 'Completed'
   group by
     f.order_id,
     f.customer_id,
@@ -46,14 +49,10 @@ select
   o.order_id,
   o.customer_id,
   c.customer_name,
-  ord.order_status,
   o.order_total_sales_amount
 
-from orders o
+from completed_orders o
 join {{ ref('dim_date') }} d
   on o.date_day = d.date_day
 join {{ ref('dim_customer') }} c
   on o.customer_id = c.customer_id
-join {{ ref('dim_orders') }} ord
-  on o.order_id = ord.order_id
-where ord.order_status = 'Completed'
