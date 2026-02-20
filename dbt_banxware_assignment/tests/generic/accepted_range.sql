4f98b386b1de36d71b69a196018124d0908340d9
Muhd-Abdullah <muhammadabdullah.e1@gmail.com>
2026-02-20 10:09:14 +0100

    dbt assignment

---
+++ dbt_banxware_assignment/tests/generic/accepted_range.sql
@@ -0,0 +1,27 @@
+{% test accepted_range(model, column_name, min_value=None, max_value=None, inclusive=True) %}
+
+select *
+from {{ model }}
+where
+(
+    {% if min_value is not none %}
+        {% if inclusive %}
+            {{ column_name }} < {{ min_value }}
+        {% else %}
+            {{ column_name }} <= {{ min_value }}
+        {% endif %}
+    {% else %}
+        false
+    {% endif %}
+
+    {% if max_value is not none %}
+        or
+        {% if inclusive %}
+            {{ column_name }} > {{ max_value }}
+        {% else %}
+            {{ column_name }} >= {{ max_value }}
+        {% endif %}
+    {% endif %}
+)
+
+{% endtest %}
